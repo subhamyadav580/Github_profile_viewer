1a7de90c14c94f3bc4c2867bb556e9bec0cec82a
Shubham Yadav <subhamyadav580@gmail.com>
2020-06-11 22:49:47 +0530

    Removing unused statements

--- gitprofile/githubdata/views.py
+++ gitprofile/githubdata/views.py
@@ -6,9 +6,7 @@ def home(request):
         username = request.POST['username']
         print(username)
         url = "https://api.github.com/users/{}".format(username)
-        print(url)
         data = requests.get(url).json()
-        print(data)
         return render(request, 'index.html',{'data':data})
     else:
         return render(request, 'index.html')
